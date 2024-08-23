c4e084d58ad73a38097aed6fcd726ad2e6126a08
d3vLimpiado <jblimpiado@gmail.com>
2024-08-23 11:20:19 +0800

    feat: update typography components

--- src/components/ui/Typography/index.tsx
+++ src/components/ui/Typography/index.tsx
@@ -1,27 +1,28 @@
-import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils";
+import { HTMLProps, PropsWithChildren } from "react";
 
-export function H1({ children }: PropsWithChildren) {
-  return <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>
+export function H1({ children, className }: PropsWithChildren<HTMLProps<HTMLHeadingElement>>) {
+  return <h1 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl", className)}>{children}</h1>
 }
 
-export function H2({ children }: PropsWithChildren) {
-  return <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">{children}</h2>
+export function H2({ children, className }: PropsWithChildren<HTMLProps<HTMLHeadingElement>>) {
+  return <h2 className={cn("scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0", className)}>{children}</h2>
 }
 
-export function H3({ children }: PropsWithChildren) {
-  return <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>
+export function H3({ children, className }: PropsWithChildren<HTMLProps<HTMLHeadingElement>>) {
+  return <h3 className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)}>{children}</h3>
 }
 
-export function Paragraph({ children }: PropsWithChildren) {
+export function Paragraph({ children, className }: PropsWithChildren<HTMLProps<HTMLParagraphElement>>) {
   return (
-    <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>
+    <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}>{children}</p>
   )
 }
 
 
-export function Blockquote({ children }: PropsWithChildren) {
+export function Blockquote({ children, className }: PropsWithChildren<HTMLProps<HTMLQuoteElement>>) {
   return (
-    <blockquote className="mt-6 border-l-2 pl-6 italic">
+    <blockquote className={cn("mt-6 border-l-2 pl-6 italic", className)}>
       {children}
     </blockquote>
   )
